0a522967fa2b89a0646904ae7d0d2120ea303248
Timothée Lebourdais <timothee.leb@icloud.com>
2023-08-09 18:41:19 +0200

    Add e2e test

--- src/adapters/primary/cli/cli.e2e.spec.ts
+++ src/adapters/primary/cli/cli.e2e.spec.ts
@@ -1,9 +1,22 @@
 import fs from "fs"
 import path from "path"
 import { compareIndexesHandler, listRedundantIndexesHandler } from "./handlers"
+import { MongoClient } from "mongodb"
 
 describe("Commands tests", () => {
-  const url = "mongodb://localhost:27017/test-db"
+  const url = "mongodb://localhost:27017/test-db-e2e"
+  const mongoClient = new MongoClient(url)
+  const db = mongoClient.db()
+
+  beforeAll(async () => {
+    await mongoClient.connect()
+    await db.dropDatabase()
+    await db.createIndex("collection1", { test: 1 })
+  })
+
+  afterAll(async () => {
+    await mongoClient.close()
+  })
 
   describe("compareIndexes", () => {
     const reportPath = "reports/compare-indexes.html"
